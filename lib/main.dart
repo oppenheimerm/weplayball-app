05c9961a05c170987c8c12fda75569103a680a03
unknown <michieloppenheimer@gmail.com>
2019-05-01 15:55:38 +0100

    app state store added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:weplayball/service/appState.dart';
 import 'lang/wpb_localizations_delegate.dart';
 import 'package:weplayball/pages/page_root.dart';
 import 'package:weplayball/service/auth/auth_provider.dart';
@@ -15,6 +16,37 @@ void main() {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
       .then((_) => runApp(new MyApp()));
 }
+
+//  new 30/04/2019
+/*class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //  wrap app in the StateContainer
+    return StateContainer(
+      auth: Auth(),
+      child: MaterialApp(
+        title: 'WePlayBall',
+        home: RootPage(),
+        theme: ThemeData(fontFamily: 'Poppins'),
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          const SitLocalizationsDelegate(),
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          // The order of this list matters. By default, if the
+          // device's locale doesn't exactly match a locale in
+          // supportedLocales then the first locale in
+          // supportedLocales with a matching
+          // Locale.languageCode is used. If that fails then the
+          // first locale in supportedLocales is used.
+          const Locale('en'),
+        ],
+      ),
+    );
+  }
+}*/
 
 class MyApp extends StatelessWidget {
   @override
